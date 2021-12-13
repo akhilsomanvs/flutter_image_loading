51a875e642d579394a414e0d7ad04aead3cc5510
akhilsomanvs <akhilsomanvs@gmail.com>
2021-12-13 18:15:55 +0530

    Added Home screen base design.

---
+++ lib/views/homeScreen/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_arch_utils/flutter_arch_utils.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  double _usableHeight = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: ResponsiveSafeArea(
+        builder: (context, size) {
+          if (_usableHeight < size.height) {
+            _usableHeight = size.height;
+          }
+          return SingleChildScrollView(
+            child: SizedBox(
+              height: _usableHeight,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
