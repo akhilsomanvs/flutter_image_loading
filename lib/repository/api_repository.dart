075690232e434ae1acb202b67f8eb42cb72c1038
akhilsomanvs <akhilsomanvs@gmail.com>
2021-12-15 16:16:42 +0530

    Replacing constants with values in API url

--- lib/repository/api_repository.dart
+++ lib/repository/api_repository.dart
@@ -3,9 +3,9 @@ import 'package:flutter_image_loading/repository/api/api_response.dart';
 import 'package:flutter_image_loading/repository/api_service.dart';
 
 class ApiRepository {
-  Future<ApiResponse> fetchImages() async {
+  Future<ApiResponse> fetchImages(String searchTerm, int page) async {
     try {
-      final response = await ApiService.getImages();
+      final response = await ApiService.getImages(searchTerm, page);
       return ApiResponse.completed(ImageApiResponse.fromJson(response));
     } catch (e) {
       return ApiResponse.error(e.toString());
--- lib/repository/api_service.dart
+++ lib/repository/api_service.dart
@@ -1,18 +1,30 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter_image_loading/api_constants.dart';
 import 'package:flutter_image_loading/repository/api/api_exception.dart';
 import 'package:flutter_image_loading/utils/app_constants.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static String _url = "https://pixabay.com/api/?key=${AppConstants.LITERAL_API_KEY}&q=${AppConstants.LITERAL_SEARCH_TERM}&image_type=photo";
+  static String _url = "https://pixabay.com/api/?key=${AppConstants.LITERAL_API_KEY}&image_type=photo";
+  static String _searchQuery = "&q=${AppConstants.LITERAL_SEARCH_TERM}";
   static String _pagination = "&page=${AppConstants.LITERAL_PAGE}&per_page=${AppConstants.LITERAL_PER_PAGE}";
 
-  static Future<dynamic> getImages() async {
+  static Future<dynamic> getImages(String searchTerm, int page, [int perPageCount = 10]) async {
     var responseJson;
     try {
-      final response = await http.get(Uri.parse(_url));
+      final String urlEncodedSearchTerm = Uri.encodeComponent(searchTerm);
+      String apiUrl = _url.replaceAll(AppConstants.LITERAL_API_KEY, API_KEY);
+      String searchQ = _searchQuery.replaceAll(AppConstants.LITERAL_SEARCH_TERM, urlEncodedSearchTerm);
+      String paginationUrl = _pagination.replaceAll(AppConstants.LITERAL_PAGE, page.toString()).replaceAll(AppConstants.LITERAL_PER_PAGE, perPageCount.toString());
+      String finalUrl = "";
+      if (searchTerm.isNotEmpty) {
+        finalUrl = apiUrl + searchQ + paginationUrl;
+      } else {
+        finalUrl = apiUrl + paginationUrl;
+      }
+      final response = await http.get(Uri.parse(finalUrl));
       responseJson = _returnResponse(response);
     } on SocketException {
       throw FetchDataException('No Internet connection');
